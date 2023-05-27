031da0e7cd32113861075615dc9ab5be243f7b9e
BinxuWang <binxu.wang@wustl.edu>
2023-05-26 19:55:55 -0500

    fix bug

--- circuit_toolkit/__init__.py
+++ circuit_toolkit/__init__.py
@@ -1,8 +1,8 @@
 
-from circuit_toolkit.utils.GAN_utils import upconvGAN
-from circuit_toolkit.utils.CNN_scorers import load_featnet, TorchScorer
-from circuit_toolkit.utils.Optimizers import CholeskyCMAES, ZOHA_Sphere_lr_euclid
-from circuit_toolkit.utils.plot_utils import saveallforms, show_imgrid, save_imgrid, save_imgrid_by_row, showimg
+from circuit_toolkit.GAN_utils import upconvGAN
+from circuit_toolkit.CNN_scorers import load_featnet, TorchScorer
+from circuit_toolkit.Optimizers import CholeskyCMAES, ZOHA_Sphere_lr_euclid
+from circuit_toolkit.plot_utils import saveallforms, show_imgrid, save_imgrid, save_imgrid_by_row, showimg
 from circuit_toolkit.grad_RF_estim import grad_RF_estimate, GAN_grad_RF_estimate, \
     gradmap2RF_square, fit_2dgauss, show_gradmap
-from circuit_toolkit.utils.stats_utils import summary_by_block
+from circuit_toolkit.stats_utils import summary_by_block
