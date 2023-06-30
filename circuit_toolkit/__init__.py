453f6bb9380e4806e548b5f9fa5037c83d6b0a57
BinxuWang <binxu.wang@wustl.edu>
2023-06-30 16:36:56 -0500

    trial update init

--- circuit_toolkit/__init__.py
+++ circuit_toolkit/__init__.py
@@ -1,11 +1,12 @@
 
-from circuit_toolkit.GAN_utils import upconvGAN
-from circuit_toolkit.GAN_invert_utils import GAN_invert, GAN_invert_with_scheduler
-from circuit_toolkit.GAN_manifold_utils import generate_sphere_grid_coords, generate_orthogonal_vectors_torch
-from circuit_toolkit.CNN_scorers import load_featnet, TorchScorer
-from circuit_toolkit.layer_hook_utils import print_specific_layer, get_module_names, featureFetcher, recursive_print
-from circuit_toolkit.Optimizers import CholeskyCMAES, ZOHA_Sphere_lr_euclid
-from circuit_toolkit.plot_utils import saveallforms, show_imgrid, save_imgrid, save_imgrid_by_row, showimg
-from circuit_toolkit.grad_RF_estim import grad_RF_estimate, GAN_grad_RF_estimate, \
+
+from .GAN_utils import upconvGAN
+from .GAN_invert_utils import GAN_invert, GAN_invert_with_scheduler
+from .GAN_manifold_utils import generate_sphere_grid_coords, generate_orthogonal_vectors_torch
+from .CNN_scorers import load_featnet, TorchScorer
+from .layer_hook_utils import print_specific_layer, get_module_names, featureFetcher, recursive_print
+from .Optimizers import CholeskyCMAES, ZOHA_Sphere_lr_euclid
+from .plot_utils import saveallforms, show_imgrid, save_imgrid, save_imgrid_by_row, showimg
+from .grad_RF_estim import grad_RF_estimate, GAN_grad_RF_estimate, \
         gradmap2RF_square, fit_2dgauss, show_gradmap
-from circuit_toolkit.stats_utils import summary_by_block
+from .stats_utils import summary_by_block
